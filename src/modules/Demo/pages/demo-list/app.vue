185f253a60a3d0e6d7263572f9cecf3b739381d4
wen911119 <wen911119@gmail.com>
2018-01-08 00:13:25 +0800

    继续完善

--- src/modules/Demo/pages/demo-list/app.vue
+++ src/modules/Demo/pages/demo-list/app.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="login-page">
         demo-list
-        <router-link to="/demo/vuex">vuex demo</router-link>        
-        <router-link to="/demo/iconfont">iconfont demo</router-link> 
+        <router-link to="/demo/vuex">vuex demo</router-link>
+        <router-link to="/demo/iconfont">iconfont demo</router-link>
         <router-link to="/demo/i18n">i18n demo</router-link>
     </div>
 </template>
 <script>
+    import { POST } from '@/interfaces/network'
     export default {
         data() {
             return {
 
             }
+        },
+        async mounted() {
+            try {
+                let ret = await POST('/test')
+                console.log(ret, 99999)
+            } catch (err) {
+                console.log(err, 44444)
+            }
+
         }
     }
 </script>
 <style>
-    
 </style>
